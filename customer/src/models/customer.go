84f656119dd3aec3095f11a9fbb753096594f749
Gabriel Nogueira <gabrielmvnogueira@gmail.com>
2023-11-04 12:43:37 -0300

    feat: add customer model

---
+++ customer/src/models/customer.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Customer struct {
+	ID        uint
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
